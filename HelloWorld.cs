0daba011372a5b9793eef860e0437263e070542b
strelkomet228 <125580662+strelkomet228@users.noreply.github.com>
2023-02-16 18:03:33 +0300

    Add files via upload
    i love your mom

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //i love your mom
+        print("Hello,world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
